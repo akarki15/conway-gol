8c226c7f4a9868117214b45538653ed7dd02b4c6
Aashish Karki <aashish@clypd.com>
2017-04-26 19:09:51 -0400

    Made vec 2d and added a func to get neighbors

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,56 @@ impl fmt::Display for Cell {
 }
 
 fn main() {
-    let mut grid: Vec<Cell> = Vec::new();
+    let mut grid = Vec::new();
     for i in 1..40 {
-        let c: Cell = Cell { alive: true };
-        grid.push(c);
+        let mut row = Vec::new();
+        for i in 1..40 {
+            let c: Cell = Cell { alive: true };
+            row.push(c);
+        }
+        grid.push(row);
     }
-    for i in 1..40 {
-        println!("{0}", grid[0]);
+    grid[1][1].alive = false;
+    for i in grid.iter() {
+        for j in i.iter() {
+            print!("{0}", j);
+        }
+        print!("\n");
+    }
+}
+
+fn get_neighbors(i: i32, j: i32, n: i32) -> Vec<Vec<i32>> {
+    let mut neighbors = Vec::new();
+    neighbors.push(vec![i - 1, j]);
+    neighbors.push(vec![i, j - 1]);
+    neighbors.push(vec![i - 1, j - 1]);
+    neighbors.push(vec![i, j + 1]);
+    neighbors.push(vec![i + 1, j + 1]);
+    neighbors.push(vec![i + 1, j]);
+    neighbors.push(vec![i + 1, j - 1]);
+    neighbors.push(vec![i - 1, j + 1]);
+
+    let mut legitNeighbor = Vec::new();
+    for neighbor in neighbors.iter() {
+        if neighbor[0] < 0 || neighbor[1] < 0 || neighbor[0] >= n || neighbor[1] >= n {
+            continue;
+        }
+        legitNeighbor.push(vec![neighbor[0], neighbor[1]]);
+    }
+    legitNeighbor
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::get_neighbors;
+
+    #[test]
+    fn test_get_neighbors() {
+        let mut neighbors = get_neighbors(0, 0, 3);
+        assert_eq!(3, neighbors.len());
+
+        neighbors = get_neighbors(1, 1, 3);
+        assert_eq!(8, neighbors.len());
     }
 }
